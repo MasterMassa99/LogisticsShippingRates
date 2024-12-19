3b3257c1cbd34f38b4c2f53748ce5d680c19b5af
EricMassa99 <eric.massa123@gmail.com>
2024-12-19 17:45:19 +0100

    Create Shipping_Cost_Calculator.py

---
+++ Shipping_Cost_Calculator.py
@@ -0,0 +1,11 @@
+# Calcolatore dei Costi  di Spedizione
+
+##Inserisci il peso del pacco e la tariffa di spedizione
+peso = flot(input("Inserisci il peso del pacco in chilogrammi:"))
+rate = flot(input("Inserisci la tariffa di spedizione al chilogrammo :"))
+
+## Calcolo il costo di spedizione
+tasso di peso del costo di spedizione 
+
+## Visualizza il risultato 
+print(f"Costo di spedizione: {shipping_cost} USD")
